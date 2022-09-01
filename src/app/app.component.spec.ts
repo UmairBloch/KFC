34d11a55fd7f12531b92bfc5254f8abb4bf8827e
DESKTOP-AR1STNE\USER <umairbaloch1889@gmail.com>
2022-09-01 13:19:57 +0500

    -setup completed

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'KFC'`, () => {
+  it(`should have as title 'project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('KFC');
+    expect(app.title).toEqual('project');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('KFC app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('project app is running!');
   });
 });
